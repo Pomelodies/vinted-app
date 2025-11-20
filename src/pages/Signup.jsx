e8805017f599466798a6a65be30a25ecd8f06ce1
Pomelodies <randomelodies@gmail.com>
2025-11-20 15:11:03 +0100

    Signup page updated

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,15 +1,70 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newsletter, setNewsletter] = useState(false);
+
+  const handleNameChange = (event) => {
+    const value = event.target.value;
+    setName(value);
+  };
+
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+  };
+
+  const handleNewsletterChange = (event) => {
+    const value = event.target.value;
+    setNewsletter(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name, email, password, newsletter);
+  };
+
   return (
     <div className="signup">
       <h2>S'inscrire</h2>
-      <form className="form-signup">
-        <input type="text" placeholder="Nom d'utilisateur" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Mot de passe" />
+      <form onSubmit={handleSubmit} className="form-signup">
+        <input
+          type="text"
+          name="name"
+          value={name}
+          placeholder="Nom d'utilisateur"
+          onChange={handleNameChange}
+        />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          placeholder="Email"
+          onChange={handleEmailChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          placeholder="Mot de passe"
+          onChange={handlePasswordChange}
+        />
         <div className="newsletter">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            name="subscribe"
+            value="true"
+            onChange={handleNewsletterChange}
+          />
           <p>S'inscrire à notre newsletter</p>
         </div>
         <p className="disclaimer">
@@ -17,7 +72,7 @@ const Signup = () => {
           Conditions et Politique de Confidentialité de Vinted. Je confirme
           avoir au moins 18 ans.
         </p>
-        <button type="submit">S'inscrire</button>
+        <button>S'inscrire</button>
         <Link to="/login">Tu as déjà un compte ? Connecte-toi !</Link>
       </form>
     </div>
