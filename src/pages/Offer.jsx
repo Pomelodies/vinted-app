7580680c4b58636bcd53dac483ce86fb14d376cd
Pomelodies <randomelodies@gmail.com>
2025-11-20 14:21:43 +0100

    signup page added

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Offer = () => {
+  const params = useParams();
+  // console.log(params); {"id": "3"}
   return (
     <main>
       <h1>Offer Container</h1>
+      <p>Cette page est la page du produit : {params.id}</p>
       <Link to="/">Go to Home page</Link>
       {/* Lien vers la page home */}
     </main>
@@ -11,3 +15,7 @@ const Offer = () => {
 };
 
 export default Offer;
+
+// Je dois interroger la base de donnée avec le params que je reçois
+// à partir de cette information, je récupère les éléments demandés
+// je les dispose sur la page
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,5 @@
+const Signup = () => {
+  return <>COUCOU !!!</>;
+};
+
+export default Signup;
